efed3dcbe4181e4ad2f1ba15da8e53d9d2ab45d3
Oliver Garcia <ogarci5@gmail.com>
2013-08-15 23:35:46 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquareSuits::Application.config.secret_token = '9fafe9babfa34c03ced04d21ab53d41b2b35f4b80e8addd6a9015d8ed7020f9e669ec517d62b442dc56c5183963e8d6751c447fa49212871254a953772e76a52'
